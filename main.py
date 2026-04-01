a69b91aa4a8bdab15dc4d1ae00ea43cf9074c6a2
qwehash510 <qwehash510@gmail.com>
2026-04-02 01:34:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telethon import TelegramClient, events, Button
 
 # ---------------- AYARLAR ----------------
 API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH"))
+API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN"))
 
 # Grup reklamı
